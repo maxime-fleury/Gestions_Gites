1a62ca84a4733ec23001757a5c9309fb0836aee8
Anthony-S58 <tonysim01@hotmail.com>
2021-06-10 11:41:00 +0200

    addpictures

--- index.php
+++ index.php
@@ -6,3 +6,16 @@ include $inc_path . "connexion.php";
 
 include $inc_path . "footer.php";
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
