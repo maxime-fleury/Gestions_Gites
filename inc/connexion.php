bc3a1ef6efb12a101bb07abbf9f647be50489f68
maxime-fleury <m.fleury@codeur.online>
2021-06-15 14:26:54 +0200

    fixed merge conflict

--- inc/connexion.php
+++ inc/connexion.php
@@ -1,14 +1,9 @@
 <?php
-<<<<<<< HEAD
-$dsn = 'mysql:dbname=gite;host=localhost';
-$user = 'root';
-$password = "password123";
-=======
+
 $db_host = "localhost";
 $db_name = "short";
 $db_user = "root";
 $db_pwd = "password123";
->>>>>>> asbranch
 $host_name = "http://xill.tk";
 
 $dbh = new PDO($dsn, $user, $password);
