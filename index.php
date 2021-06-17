cc0e1ef31a8b0baa75e48122d7f0cb2fd5cb58e7
Anthony-S58 <tonysim01@hotmail.com>
2021-06-17 15:44:42 +0200

    modifview

--- index.php
+++ index.php
@@ -24,59 +24,93 @@ include $inc_path . "connexion.php";
             <div class="view">
                 
                 <div class="font">
+                    <span id="id1" class="target">
+                    </span>
+                    <span id="id2" class="target">
+                    </span>
+                    <span id="id3" class="target">
+                    </span>
+                    <span id="id4" class="target">
+                    </span>
+                    <div class="cadre_diapo">
+                        <div class="interieur_diapo">
+                            <div class=description>
+                                <img src="img/gite2p1.jpg" height="250px" width="600px"alt>
+                            </div>
+                            <div class=description>
+                                <img src="img/gite2p2.jpg" height="250px" width="600px"alt>
+                            </div>
+                            <div class=description>
+                                <img src="img/gite2p3.jpg" height="250px" width="600px"alt>
+                            </div>
+                            <div class=description>
+                                <img src="img/gite2p4.jpg" height="250px" width="600px"alt>
+                            </div>
+                        </div>
+                        <ul class="navigation_diapo">
+                            <li>
+                                <a href="#id1">
+                                <img src="img/gite2p1.jpg" height="100px" width="140px" alt>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#id2">
+                                <img src="img/gite2p2.jpg" height="100px" width="140px" alt>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#id3">
+                                <img src="img/gite2p3.jpg" height="100px" width="140px" alt>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#id4">
+                                <img src="img/gite2p4.jpg" height="100px" width="140px" alt>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="logos">
 
+                            <div class="logo"><img src="img/bed.png" height="30px" width="30px"><p>1</p></div>
+                            <div class="logo"><img src="img/user-group.png" height="30px" width="30px"><p>2</p></div>
+                            <div class="logo"><img src="img/bathtub.png" height="30px" width="30px"><p>1</p></div>
+                            <div class="logo"><img src="img/swimming.png" height="30px" width="30px"><p>non</p></div>
+                            <div class="logo"><img src="img/dog.png" height="30px" width="30px"><p>non</p></div>
+                            <div class="logo"><img src="img/wifi.png" height="30px" width="30px"><p>oui</p></div>
+
+                        </div>
+                        <div class="localisation">
+                            <h2>Lyon</h2>
+                            <p><b>Logement entier</b><br>Vous aurez l'appartement rien que pour vous.</p>
+                            <p><b>Arrivée autonome</b><br> Vous pouvez entrer dans les lieux avec une boîte à clé sécurisée.</p>
+                            <p><b>Nettoyage renforcé</b><br>Cet hôte s'engage à appliquer le processus de nettoyage renforcé.</p>
+                            
+                        </div>
+                    </div>
+                    
                     
                     <div class="pic">
-                        <h2>Nevers</h2>
-                        <p><b>Logement entier</b><br>Vous aurez l'appartement rien que pour vous.</p>
-                        <p><b>Arrivée autonome</b><br> Vous pouvez entrer dans les lieux avec une boîte à clé sécurisée.</p>
-                        <p><b>Nettoyage renforcé</b><br>Cet hôte s'engage à appliquer le processus de nettoyage renforcé.</p>
-                            <hr>
-                            <br>
+                        
                         <form action="">
-                            <h2><b>60€</b>/nuit</h2>
+                            <h2><b>200€</b>/nuit</h2>
 
                             <label for="">Du :</label>
                             <input type="date" name="" id="">
-
+                            <br>
                             <label for="">Au :</label>
                             <input type="date" name="" id="">
 
                             <br>
                             <input type="number" placeholder="Nombre de personnes" name="" id="">
 
-                            <h2>Résumé de votre réservation :<br>
-                                <p>2 nuits à 60€</p>
-                            </h2>
-                            <h2><b>Total :</b>&nbsp;&nbsp;<mark>120€</mark></h2>
+                            <h2>Résumé de votre réservation :<br></h2>
+                                <h2>2 nuits à 200€</h2>
+                            
+                            <h2><b>Total :</b>&nbsp;&nbsp;<mark>400€</mark></h2>
                             <button type="submit">Réserver</button>
                         
                         </form>
    
-                    </div>
-
-                    <div class= "slider">
-
-                        <img src="img/gite2p1.jpg" width="100%" height="50%">
-                            <div class="logos">
-
-                                <div class="logo"><img src="img/bed.png" height="30px" width="30px"><p>1</p></div>
-                                <div class="logo"><img src="img/user-group.png" height="30px" width="30px"><p>2</p></div>
-                                <div class="logo"><img src="img/bathtub.png" height="30px" width="30px"><p>1</p></div>
-                                <div class="logo"><img src="img/swimming.png" height="30px" width="30px"><p>non</p></div>
-                                <div class="logo"><img src="img/dog.png" height="30px" width="30px"><p>non</p></div>
-                                <div class="logo"><img src="img/wifi.png" height="30px" width="30px"><p>oui</p></div>
-
-                            </div>
-
-                            <div class="slide">
-
-                                <img src="img/gite2p2.jpg" alt="" height="220px" width="250px">
-                                <img src="img/gite2p3.jpg" alt="" height="220px" width="250px">
-                                <img src="img/gite2p4.jpg" alt="" height="220px" width="250px">
-
-                            </div>
-
                     </div>
 
                     <div class="rond">X</div>                    
@@ -136,9 +170,9 @@ include $inc_path . "connexion.php";
 
                     </div>
 
-                    <div class="descript"><h2>Lyon</h2><p>A partir de <b>90€</b> par jour</p></div>
+                    <div class="descript"><h2>Lyon</h2><p>A partir de <b>200€</b> par jour</p></div>
                     <div class="zoom">
-                        <button>Voir le logement</button>
+                        <button id="test">Voir le logement</button>
                     </div>
                     
                 </div>
@@ -165,7 +199,7 @@ include $inc_path . "connexion.php";
 
                     <div class="descript"><h2>Cannes</h2><p>A partir de <b>130€</b> par jour</p></div>
                     <div class="zoom">
-                        <button id="test">Voir le logement</button>
+                        <button>Voir le logement</button>
                     </div>
                   
                 </div>
